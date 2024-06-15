5d66b00b57706e96ce0db22b52aecfed43604fc7
gregory7635 <gregory.vasiliev@gmail.com>
2024-06-15 13:36:46 +0300

    2 commit

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -7,3 +7,17 @@ private:
 	int minute;
 	int second;
 public:
+	void get() {
+		cin >> hour;
+		cin >> minute;
+		cin >> second;
+		cout << "Введенные данные: часы- " << hour << " " << "минуты- " << minute << " " << "секунды- " << second << " " << endl;
+	}
+	void function1() {
+		double res = (hour * 60) + minute + (second / 60);
+		if (hour < 24) {
+			int minutesUntilMidnight = (24 * 60) - res;
+			cout << "До полуночи осталось " << minutesUntilMidnight << " минут" << std::endl;
+		}
+		else {
+			cout << "Уже полночь" << std::endl;
