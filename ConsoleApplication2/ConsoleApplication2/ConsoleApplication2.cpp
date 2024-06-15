2688d825b35d71827f694f16af3d367684ca02ec
gregory7635 <gregory.vasiliev@gmail.com>
2024-06-15 14:42:02 +0300

    commit 5 v1.4+console2

---
+++ ConsoleApplication2/ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,21 @@
+﻿
+#include <iostream>
+﻿#include <iostream>
+#include <list>
+using namespace std;
+void duplicateOddValues(list<int>& lst) {
+    list<int>::iterator iter = lst.begin();
+    while (iter != lst.end()) {
+        if (*iter % 2 != 0) {
+            lst.insert(iter, *iter);
+            iter++;
+        }
+        else {
+            iter++;
+        }
+    }
+}
+
+int main() {
+    setlocale(LC_ALL, "RU");
+
